788cadcc5cbc2e2886aa27af078a45d6ebd5fb1a
wmerrow <william.merrow@gmail.com>
2021-03-22 00:33:44 -0400

    udpate bubble chart

--- charts.js
+++ charts.js
@@ -4,8 +4,11 @@ d3.queue()
 .defer(d3.csv, "data/output/party_control.csv")
 //.defer(d3.csv, "data/parties_first-last.csv")
 .await(function(error, data_all) {
-    if (error) throw error;
+  if (error) throw error;
 
+  /// remove nebraska for now
+  var data_all = data_all
+    .filter(({state}) => state !== "Nebraska");
 
   // convert columns to numeric
   data_all.forEach(function(data){
@@ -71,19 +74,18 @@ d3.queue()
       .append('svg')
         .attrs({width: width, height: height})
 
-    // nodes
-
-    // Initialize the circle: all located at the center of the svg area
+    // set up circles: all located at the center of the svg area
     var node = svg.append("g")
       .selectAll("circle")
       .data(data21)
       .enter()
       .append("circle")
-        .attr("r", d=> size(d.pop))
-        .attr("cx", width / 2)
-        .attr("cy", height / 2)
-        .style("fill", d=> color(d.government_cont_text));
+      .attr("r", d=> size(d.pop))
+      .attr("cx", width / 2)
+      .attr("cy", height / 2)
+      .style("fill", d=> color(d.government_cont_text));
 
+    // set up labels
     var label = svg.append("g")
       .selectAll("text")
       .data(data21)
@@ -95,29 +97,7 @@ d3.queue()
       .attr("x", width / 2)
       .attr("y", height / 2);
 
-    // Features of the forces applied to the nodes:
-    var simulation = d3.forceSimulation()
-        .force("x", d3.forceX().strength(0.5).x( function(d){ return x(d.government_cont_text) } ))
-        .force("y", d3.forceY().strength(0.1).y( height/2 ))
-        .force("center", d3.forceCenter().x(width / 2).y(height / 2)) // Attraction to the center of the svg area
-        .force("charge", d3.forceManyBody().strength(0.5)) // Nodes are attracted one each other of value is > 0
-        .force("collide", d3.forceCollide().strength(.1).radius(d=> size(d.pop) + nodePadding).iterations(1)) // Force that avoids circle overlapping
 
-    // Apply these forces to the nodes and update their positions.
-    // Once the force algorithm is happy with positions ('alpha' value is low enough), simulations will stop.
-    simulation
-        .nodes(data21)
-        .on("tick", function(d){
-          node
-              .attr("cx", function(d){ return d.x; })
-              .attr("cy", function(d){ return d.y; })
-          label
-              .attr("x", function(d){ return d.x; })
-              .attr("y", function(d){ return d.y + textSize / 2 - 2; })
-        });
-
-    // labels
-    // simulation to position them 
 
     var gs = d3.graphScroll()
         .container(d3.select('.container-1'))
@@ -126,6 +106,30 @@ d3.queue()
         .sections(d3.selectAll('.container-1 #sections > div'))
         // .offset(innerWidth < 900 ? innerHeight - 30 : 200)
         .on('active', function(i){
+            // var x = 0;
+            // var y = 0;
+
+          // Features of the forces applied to the nodes:
+          var simulation = d3.forceSimulation()
+              .force("x", d3.forceX().strength(0.5).x( function(d){ return x(d.government_cont_text) } ))
+              .force("y", d3.forceY().strength(0.1).y( height/2 ))
+              .force("center", d3.forceCenter().x(width / 2).y(height / 2)) // Attraction to the center of the svg area
+              .force("charge", d3.forceManyBody().strength(0.5)) // Nodes are attracted one each other of value is > 0
+              .force("collide", d3.forceCollide().strength(0.1).radius(d=> size(d.pop) + nodePadding).iterations(1)) // Force that avoids circle overlapping
+
+          // Apply these forces to the nodes and update their positions.
+          // Once the force algorithm is happy with positions ('alpha' value is low enough), simulations will stop.
+          simulation
+              .nodes(data21)
+              .on("tick", function(d){
+                node
+                    .attr("cx", function(d){ return d.x; })
+                    .attr("cy", function(d){ return d.y; })
+                label
+                    .attr("x", function(d){ return d.x; })
+                    .attr("y", function(d){ return d.y + textSize / 2 - 2; })
+              });
+
 
         })
 
